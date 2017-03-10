c73d364e4bfe8e95bbf268ff4fc93c092c79f059
vaneguz <vaneguz27@gmail.com>
2017-03-10 18:17:29 -0500

    fil2

---
+++ Ejercicio1/js/script.js
@@ -0,0 +1,8 @@
+//function mellamaron(){
+//    alert(document.getElementById("nombre").value);
+//}
+
+function mellamaron(){
+    document.getElementById("jm").innerHTML = "wtf";
+    return false;
+}
